09f51cb5ad502346761549545b83424e56f21ed1
Hugo Saraiva do Nascimento <hugo@hsaraiva.com>
2024-12-07 19:16:47 -0300

    Fixing vim language setting for debian

--- init.lua
+++ init.lua
@@ -84,7 +84,7 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 -- Set default language to english
-vim.api.nvim_exec2('language en_US', {})
+vim.api.nvim_exec2('language en_US.utf8', {})
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
